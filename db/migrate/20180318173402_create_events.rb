0a9513ca85beb15235b356cb437f5f54a048cee9
Juozas Beniusis <beniusij@gmail.com>
2018-03-19 01:11:26 +0000

    Trying to migrate with existing table.

--- db/migrate/20180318173402_create_events.rb
+++ db/migrate/20180318173402_create_events.rb
@@ -1,6 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
+      t.string  :place
+      t.date    :date, null: false
+      t.time    :time
+      t.text    :keywords
 
       t.timestamps
     end
