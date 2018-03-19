0d3e03e912350b36407f579246ba65f1f75fa4c0
Juozas Beniusis <beniusij@gmail.com>
2018-03-19 18:31:55 +0000

    Showing list in Bham and other.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
 class MainController < ApplicationController
   def home
+    location = "Birmingham"
+    @local_events = Event.where("keywords LIKE ?", "%" + location + "%").order(date: :asc)
+    @other_events = Event.where("keywords NOT LIKE ?", "%" + location + "%").order(date: :asc)
+
   end
 
   def search
