2e4a52afa102e6051b9f621c17bc97c2d8d2829c
Juozas Beniusis <beniusij@gmail.com>
2018-03-20 00:49:42 +0000

    Added About page, which still needs content.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
   validates :date, :keywords, presence: true
+
   def self.search(search)
     date = search[:date]
     keywords = search[:keywords]
     if !date.empty? && !keywords.empty?
       where("(keywords LIKE ? OR place LIKE ?) " + "AND date = ?",
-            "%" + keywords + "%", "%" + keywords + "%",
-            date).order(date: :asc)
+            "%" + keywords + "%", "%" + keywords + "%", date).order(date: :asc)
     elsif !date.empty?
       where("date = ?", date).order(place: :asc)
     elsif !keywords.empty?
